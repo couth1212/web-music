03e64090a5af7926435413b51d1dcbcae526c65d
couth1212 <18093329352@153.com>
2017-12-22 10:19:21 +0800

    three

--- music.py
+++ music.py
@@ -1,7 +1,11 @@
 import requests
 import re
 
-def main():
-    '''http://www.htqyy.com/genre/musicList/1?pageIndex=1&pageSize=20&order=hot'''
+def getHTML():
+    url = '''http://www.htqyy.com/genre/musicList/1?pageIndex=1&pageSize=20&order=hot'''
+    html = requests.get(url)
+    print(html.text)
+
+    
 if __name__ == '__main__':
-    main()
+    getHTML()
